8d330af67cf2dbf301a6767d9357059c7397a44a
Camilla <caru0001@stud.kea.dk>
2024-09-16 21:09:02 +0200

    git

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+};
